8448992fab153a458576509977e3a672d03587c2
Jurfest <jurfest.apps@gmail.com>
2022-05-26 19:09:51 -0300

    chore: config angular material for videogame-tracker app

--- apps/videogame-tracker/src/app/app.module.ts
+++ apps/videogame-tracker/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
@@ -15,12 +16,13 @@ import { APP_ROUTES } from './app.routes';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     StoreModule.forRoot({}),
     RouterModule.forRoot(APP_ROUTES),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
     EffectsModule.forRoot(),
-    VideogameTrackerFeatureGamesModule
+    VideogameTrackerFeatureGamesModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
