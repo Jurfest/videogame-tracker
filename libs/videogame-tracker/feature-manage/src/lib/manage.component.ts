d0cb0a576a6bae8d13b87de9e135512c031b3f49
Jurfest <jurfest.apps@gmail.com>
2022-06-01 18:32:58 -0300

    feat: dinamically enable/disable completed and dateOfCompetion form controls

--- libs/videogame-tracker/feature-manage/src/lib/manage.component.ts
+++ libs/videogame-tracker/feature-manage/src/lib/manage.component.ts
@@ -38,11 +38,11 @@ export class ManageComponent implements OnInit {
     console: ['', Validators.required],
   });
   gameCompletedForm = this.fb.group({
-    completed: [null, Validators.required],
+    completed: [{ value: null, disabled: true }, Validators.required],
   });
   gameDateOfCompletionForm = this.fb.group({
     // TODO: - Check validators and masks
-    dateOfCompletion: ['', Validators.required],
+    dateOfCompletion: [{ value: '', disabled: true }, Validators.required],
   });
   gamePersonalNotesForm = this.fb.group({
     personalNotes: [
@@ -51,6 +51,7 @@ export class ManageComponent implements OnInit {
     ],
   });
 
+  // UI control
   stepperOrientation: Observable<StepperOrientation>;
 
   consoleList$ = this.consoleFacade.allConsole$;
@@ -74,15 +75,39 @@ export class ManageComponent implements OnInit {
     this.consoleFacade.loadConsoleList();
   }
 
+  onYearSubmit(): void {
+    const year = Number(this.gameYearForm.controls['year'].value);
+    this.setMinDateRange(year);
+    this.updateDisabledCompleted(year);
+  }
+
   // Set the minimum date to January 1st of game release year
-  setMinDateRange(): void {
-    const gameReleaseYear = moment(
-      this.gameYearForm.controls['year'].value
-    ).get('year');
+  private setMinDateRange(gameReleaseYear: number): void {
     this.minDate = new Date(gameReleaseYear - 0, 0, 1);
 
     // FIXME: - Reverse flux test condition where completion date is invalid
     // after year change and fix by cleaning invalid completion date
+  }
+
+  private updateDisabledCompleted(year: number | undefined): void {
+    const disabledCompleted = year ? year > moment().get('y') : false;
+
+    if (disabledCompleted) {
+      this.gameCompletedForm.controls['completed'].disable();
+    } else {
+      this.gameCompletedForm.controls['completed'].enable();
+    }
+  }
+
+  updateDisabledCompletionDate(): void {
+    const disabledCompletionDate =
+      !this.gameCompletedForm.controls['completed'].value;
+
+    if (disabledCompletionDate) {
+      this.gameDateOfCompletionForm.controls['dateOfCompletion'].disable();
+    } else {
+      this.gameDateOfCompletionForm.controls['dateOfCompletion'].enable();
+    }
   }
 
   addNewGame(): void {
