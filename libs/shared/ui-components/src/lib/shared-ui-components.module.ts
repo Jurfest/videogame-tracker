8a30d9b691e134006bae0c907a5227f5a8b5e041
Jurfest <jurfest.apps@gmail.com>
2022-06-01 21:13:41 -0300

    fix: add MatButtonModule to change button appearence

--- libs/shared/ui-components/src/lib/shared-ui-components.module.ts
+++ libs/shared/ui-components/src/lib/shared-ui-components.module.ts
@@ -2,11 +2,17 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatButtonModule } from '@angular/material/button';
 
 import { CardListComponent } from './components/card-list/card-list.component';
 
 @NgModule({
-  imports: [CommonModule, MatCardModule, DragDropModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    DragDropModule,
+    MatButtonModule,
+  ],
   declarations: [CardListComponent],
   exports: [CardListComponent],
 })
