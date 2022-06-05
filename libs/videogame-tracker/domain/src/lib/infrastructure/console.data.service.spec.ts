a683583707d169e8d28bc19189233934b89b3925
Jurfest <jurfest.apps@gmail.com>
2022-06-05 19:53:49 -0300

    test: verify if load retrieve all data

--- libs/videogame-tracker/domain/src/lib/infrastructure/console.data.service.spec.ts
+++ libs/videogame-tracker/domain/src/lib/infrastructure/console.data.service.spec.ts
@@ -1,16 +1,71 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ConsoleEntity } from '../entities/console.models';
 
 import { ConsoleDataService } from './console.data.service';
 
-describe('Console.DataService', () => {
-  let service: ConsoleDataService;
+const mockConsoles: ConsoleEntity[] = [
+  {
+    id: 0,
+    name: 'PS2',
+  },
+  {
+    id: 1,
+    name: 'PS4',
+  },
+  {
+    id: 2,
+    name: 'PS5',
+  },
+  {
+    id: 3,
+    name: 'XBOX ONE',
+  },
+  {
+    id: 4,
+    name: 'XBOX SERIES X',
+  },
+  {
+    id: 5,
+    name: 'NINTENDO SWITCH',
+  },
+];
+
+describe('ConsoleDataService', () => {
+  let consoleDataservice: ConsoleDataService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ConsoleDataService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, BrowserAnimationsModule],
+      providers: [ConsoleDataService],
+    });
+    consoleDataservice = TestBed.inject(ConsoleDataService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(consoleDataservice).toBeTruthy();
+  });
+
+  it('should retrieve all data', () => {
+    consoleDataservice.load().subscribe((consoleList) => {
+      expect(consoleList).toBeTruthy();
+      expect(consoleList.length).toBe(6);
+      expect(consoleList.find((console) => console.id === 5).name).toBe(
+        'NINTENDO SWITCH'
+      );
+    });
+
+    const req = httpTestingController.expectOne(
+      'http://localhost:3001/consoles'
+    );
+    expect(req.request.method).toEqual('GET');
+    req.flush(mockConsoles);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 });
