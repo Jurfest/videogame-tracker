08a61a0e5cd1a65eef5e0433a04efc31dd0d0965
Jurfest <jurfest.apps@gmail.com>
2022-06-05 22:06:59 -0300

    test: coverage navbar component

--- apps/videogame-tracker/src/app/components/navbar/navbar.component.spec.ts
+++ apps/videogame-tracker/src/app/components/navbar/navbar.component.spec.ts
@@ -1,6 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, ComponentFixtureAutoDetect, TestBed } from '@angular/core/testing';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { By } from '@angular/platform-browser';
 
 import { NavbarComponent } from './navbar.component';
 
@@ -11,7 +12,7 @@ describe('NavbarComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [NavbarComponent],
-      imports: [MatToolbarModule, MatIconModule, ]
+      imports: [MatToolbarModule, MatIconModule],
     }).compileComponents();
   });
 
@@ -23,5 +24,15 @@ describe('NavbarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit event when logout matIcon inside btn is clicked', () => {
+    const spyLogoutEmitter = jest.spyOn(component.logout, 'emit');
+    const matIcon = fixture.debugElement.query(By.css('mat-icon'))
+      .nativeElement as HTMLButtonElement;
+    matIcon.click();
+
+    expect(spyLogoutEmitter).toHaveBeenCalled();
+    expect(spyLogoutEmitter).toHaveBeenCalledWith(null);
   });
 });
