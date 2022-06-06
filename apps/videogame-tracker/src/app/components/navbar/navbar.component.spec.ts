875c8c64ca485f47fbfc9eeffd14ca1875e6fd03
Jurfest <jurfest.apps@gmail.com>
2022-06-05 22:07:45 -0300

    chore: remove unused import

--- apps/videogame-tracker/src/app/components/navbar/navbar.component.spec.ts
+++ apps/videogame-tracker/src/app/components/navbar/navbar.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, ComponentFixtureAutoDetect, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { By } from '@angular/platform-browser';
